9f1dc2fa4fa5a7fbf41b9bc7056ba2164def2f2b
Nazar Ilamanov <nazar.ilamanov@gmail.com>
2022-09-23 14:51:40 -0700

    add test for hammingDistance calculation

---
+++ test/Attacks.t.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/generals/ConsecutiveShooterGeneral.sol";
+import "../src/utils/Attacks.sol";
+
+contract AttacksTest is Test {
+    using Attacks for uint256;
+
+    function setUp() public {}
+
+    function testHammingDistance64(uint256 x) public {
+        x &= 0xFFFFFFFFFFFFFFFF;
+        assertEq(x.hammingDistance64(), hammingDistance64Correct(x));
+    }
+
+    function hammingDistance64Correct(uint256 x)
+        private
+        pure
+        returns (uint256 countOfOnes)
+    {
+        for (uint256 i = 0; i < 64; i++) {
+            countOfOnes += ((x >> i) & 1);
+        }
+    }
+}
